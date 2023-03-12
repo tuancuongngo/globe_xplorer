7e4df7947fb3d35ae87125aa4d7b5928ce54f74a
tuancuongngo <70221527+tuancuongngo@users.noreply.github.com>
2023-03-12 11:08:30 -0400

    Add popup on double click to add pin

--- client/globe_xplorer/src/App.js
+++ client/globe_xplorer/src/App.js
@@ -9,12 +9,23 @@ import {format} from "timeago.js"
 
 function App() {
 
-  const [pins, setPins] = React.useState([]);
-  const [viewPort, setViewPort] = React.useState({
+  const [pins, setPins] = React.useState([]); // Set pre-existing pins
+  const [viewPort, setViewPort] = React.useState({ // Set zoom size of pins
     longitude : 12.4,
     latitude : 37.8,
     zoom : 14 
   });
+
+  const [newPlace, setNewPlace] = React.useState(null); // Set new pins with mouse-click
+  const handleAddClick = (e) => {
+    let lat = e.lngLat.lat;
+    let long = e.lngLat.lng;
+
+    setNewPlace({
+      lat : lat,
+      long : long
+    });
+  }
 
   const [currentPlaceId, setCurrentPlaceId] = React.useState(null);
 
@@ -23,6 +34,16 @@ function App() {
     console.log(long);
     setCurrentPlaceId(id);
   }
+
+  const handlePinSubmit = () => {
+
+  }
+
+  const[title, setTitle] = React.useState(null);
+  const[desc, setDesc] = React.useState(null);
+  const[rating, setRating] = React.useState(1);
+
+
 
   React.useEffect(() => {
     const getPins = async() => {
@@ -51,8 +72,10 @@ function App() {
             mapboxAccessToken = {[process.env.REACT_APP_TOKEN]}
             style = {{width:"100vw", height:"100vh"}}
             mapStyle = "mapbox://styles/ngoct/clf4kfkc4004w01p7nl01fbw3"
+            onDblClick = {handleAddClick}
             >
               <NavigationControl/>
+
               {
                 pins.map(p => (
                   <>
@@ -104,6 +127,46 @@ function App() {
                   </>
                 ))
               }
+
+              {
+                newPlace && 
+                <Popup
+                longitude = {newPlace.long}
+                latitude = {newPlace.lat}
+                closeOnClick = {false}
+                closeOnMove = {false}
+                onClose = {() => setNewPlace(null)}
+                anchor = "left"
+                >
+                  <form onSubmit={handlePinSubmit()}>
+                    <label>Title</label>
+                    <input
+                    placeholder="Enter Title"
+                    onChange={(e) => setTitle(e.target.value)}
+                    />
+
+                    <label>Review</label>
+                    <textarea
+                    placeholder="Leave a Review"
+                    onChange={(e) => setDesc(e.target.value)}
+                    />
+
+                    <label>Rating</label>
+                    <select onChange={(e) => setRating(e.target.value)}>
+                      <option value = "1">1</option>
+                      <option value = "2">2</option>
+                      <option value = "3">3</option>
+                      <option value = "4">4</option>
+                      <option value = "5">5</option>
+                    </select>
+
+                    <button className="submitButton" type="submit">Add Pin</button>
+
+                  </form>
+
+                </Popup>
+              }
+
             </Map>
         </div>
     );
